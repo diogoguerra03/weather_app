69b5394dd053b8fcd62e90eb849bc9c3aa2ff949
Diogo Guerra <63842251+diogoguerra03@users.noreply.github.com>
2025-06-24 20:24:51 +0100

    feat: clean text when search bar has text

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomSearchBar extends StatelessWidget {
+class CustomSearchBar extends StatefulWidget {
   final String hint;
   final ValueChanged<String>? onChanged;
   final VoidCallback? onSearch;
@@ -13,19 +13,45 @@ class CustomSearchBar extends StatelessWidget {
   });
 
   @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: TextField(
-        onChanged: onChanged,
+        controller: _controller,
+        onChanged: (value) {
+          setState(() {});
+          widget.onChanged?.call(value);
+        },
         decoration: InputDecoration(
-          hintText: hint,
+          hintText: widget.hint,
           hintStyle: const TextStyle(color: Colors.white54),
           prefixIcon: const Icon(Icons.search, color: Colors.white54),
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.my_location, color: Colors.white54),
-            onPressed: onSearch,
-          ),
+          suffixIcon: _controller.text.isEmpty
+              ? IconButton(
+                  icon: const Icon(Icons.my_location, color: Colors.white54),
+                  onPressed: widget.onSearch,
+                )
+              : IconButton(
+                  icon: const Icon(Icons.clear, color: Colors.white54),
+                  onPressed: () {
+                    _controller.clear();
+                    setState(() {});
+                    widget.onChanged?.call('');
+                  },
+                ),
           filled: true,
           fillColor: Colors.white12,
           border: OutlineInputBorder(
